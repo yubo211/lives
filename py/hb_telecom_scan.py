cfe492f35e010c3fa225335ad678e10bd8770c70
yubo211 <linyubo211@outlook.com>
2026-03-30 08:37:14 +0800

    Update hb_telecom_scan.py

--- py/hb_telecom_scan.py
+++ py/hb_telecom_scan.py
@@ -12,7 +12,7 @@ from tqdm import tqdm
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 # --- 配置 ---
-TARGET_PREFIX = "42.23"
+TARGET_PREFIX = "42.231"
 TARGET_PORT = 9901
 CHECK_PATH = "/iptv/live/1000.json?key=txipt"
 M3U_FILE = "py/hb_telecom.m3u"
